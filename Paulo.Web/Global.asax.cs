c38a0abdd3ca2e5a68a5b8bc028682ecf0b80c7e
Paulo Monteiro <monteiro@easycomtec.com>
2018-09-21 01:16:47 -0300

    removed unused js

--- Paulo.Web/Global.asax.cs
+++ Paulo.Web/Global.asax.cs
@@ -2,7 +2,9 @@
 using Paulo.Web.AutoMapper;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -20,6 +22,14 @@ namespace Paulo.Web
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             SimpleInjectorInitializer.Initializer();
             AutoMapperConfig.RegisterMappings();
+        }
+        
+        protected void Application_AcquireRequestState(object sender, EventArgs e)
+        {
+             CultureInfo ci = new CultureInfo("pt-BR");
+
+             Thread.CurrentThread.CurrentUICulture = ci;
+             Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(ci.Name);
         }
     }
 }
